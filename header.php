8f35b98df33890b272d72469071dcd436504e239
Amarilis Camargo <amariliscamargo@gmail.com>
2015-09-14 16:29:17 -0300

    -> Adicionei link ao menu; -> Criei pasta images -> Adicionei imagens a pasta images, para o slider(img1, img2,img3) -> Adicionei o arquivo carrossel.css

--- header.php
+++ header.php
@@ -5,6 +5,8 @@
 	<meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
 	<title> Site para teste</title>
 	<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+	<!-- Custom styles for this template -->
+    <link href="../css/carousel.css" rel="stylesheet">
 	</head>
 	<body>
 	<header class="navbar-wrapper">
@@ -18,11 +20,11 @@
 		                	<span class="icon-bar"></span>
 		                	<span class="icon-bar"></span>
 		              	</button>
-		              	<a class="navbar-brand" href="#">Project name</a>
+		              	<a class="navbar-brand" href="#">Site Teste da Amarilis</a>
 		            </div>
 		            <div id="navbar" class="navbar-collapse collapse">
 		              	<ul class="nav navbar-nav">
-		                	<li class="active"><a href="#">Home</a></li>
+		                	<li class="active"><a href="../index.php">Home</a></li>
 		                	<li><a href="#about">About</a></li>
 		                	<li><a href="#contact">Contact</a></li>
 		                	<li class="dropdown">
