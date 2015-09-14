a4927fe53adf837bd058be611aefa897afc0d267
Amarilis Camargo <amariliscamargo@gmail.com>
2015-09-14 15:10:43 -0300

    inclui os arquivos head.php e footer.php ao index.php

--- index.php
+++ index.php
@@ -1,10 +1,4 @@
-<!doctype html>
-<html lang="pt-BR">
-	<head>
-	<meta charset="UTF-8">
-	<title> Site para teste</title>
-	</head>
-	<body>
-	
-	</body>
-</html>
+<?php
+include 'header.php';
+include 'footer.php';
+?>
