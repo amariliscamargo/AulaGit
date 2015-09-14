5e827a6a290776b2f6f10f1ec46c4c38de5cad25
Amarilis Camargo <amariliscamargo@gmail.com>
2015-09-14 17:32:22 -0300

    teste.php

--- header.php
+++ header.php
@@ -25,8 +25,8 @@
 		            <div id="navbar" class="navbar-collapse collapse">
 		              	<ul class="nav navbar-nav">
 		                	<li class="active"><a href="../index.php">Home</a></li>
-		                	<li><a href="#about">About</a></li>
-		                	<li><a href="#contact">Contact</a></li>
+		                	<li><a href="../quemsomos.php">Quem somos</a></li>
+		                	<li><a href="../contato.php">Contato</a></li>
 		                	<li class="dropdown">
 		                  	<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
 				                <ul class="dropdown-menu">
--- index.php
+++ index.php
@@ -4,9 +4,9 @@
 <div id="myCarousel" class="carousel slide" data-ride="carousel">
   <!-- Indicators -->
   <ol class="carousel-indicators">
-    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-    <li data-target="#myCarousel" data-slide-to="1"></li>
-    <li data-target="#myCarousel" data-slide-to="2"></li>
+  <li data-target="#Carousel" data-slide-to="0" class="active"></li>
+  <li data-target="#Carousel" data-slide-to="1"></li>
+  <li data-target="#Carousel" data-slide-to="2"></li>
   </ol>
   <div class="carousel-inner" role="listbox">
     <div class="item active">
@@ -15,7 +15,7 @@
         <div class="carousel-caption">
           <h1>Example headline.</h1>
           <p>Note: If you're viewing this page via a <code>file://</code> URL, the "next" and "previous" Glyphicon buttons on the left and right might not load/display properly due to web browser security rules.</p>
-          <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
+          <p><a class="btn btn-lg btn-primary" href="#" role="button">Assine hoje</a></p>
         </div>
       </div>
     </div>
@@ -25,7 +25,7 @@
         <div class="carousel-caption">
           <h1>Another example headline.</h1>
           <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-          <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
+          <p><a class="btn btn-lg btn-primary" href="#" role="button">Saiba Mais</a></p>
         </div>
       </div>
     </div>
@@ -35,7 +35,7 @@
         <div class="carousel-caption">
           <h1>One more for good measure.</h1>
           <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-          <p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+          <p><a class="btn btn-lg btn-primary" href="#" role="button">Galeria de Fotos</a></p>
         </div>
       </div>
     </div>
@@ -50,4 +50,31 @@
   </a>
 </div><!-- /.carousel -->
 
-<php include 'footer.php'; ?>
+<!-- Marketing messaging and featurettes
+    ================================================== -->
+    <!-- Wrap the rest of the page in another container to center all the content. -->
+
+    <div class="container marketing">
+
+      <!-- Three columns of text below the carousel -->
+      <div class="row">
+        <div class="col-lg-4">
+          <img class="img-circle" src="../images/casinha.jpg" alt="Generic placeholder image" width="140" height="140">
+          <h2>Heading</h2>
+          <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+          <p><a class="btn btn-default" href="#" role="button">Saiba Mais &raquo;</a></p>
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-4">
+          <img class="img-circle" src="../images/casinha2.jpg" alt="Generic placeholder image" width="140" height="140">
+          <h2>Heading</h2>
+          <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
+          <p><a class="btn btn-default" href="#" role="button">Saiba Mais &raquo;</a></p>
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-4">
+          <img class="img-circle" src="../images/casinha3.jpg" alt="Generic placeholder image" width="140" height="140">
+          <h2>Heading</h2>
+          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+          <p><a class="btn btn-default" href="#" role="button">Saiba Mais &raquo;</a></p>
+        </div><!-- /.col-lg-4 -->
+      </div><!-- /.row -->
+<?php include 'footer.php'; ?>
