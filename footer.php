2bf6050fe1d728050c35e9def8d38e9ae271a0a9
Amarilis Camargo <amariliscamargo@gmail.com>
2015-10-19 12:05:50 -0200

    Modifiquei o rodapé

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
         </div>
               <footer class="navbar-static-bottom">
                 <p class="pull-right"><a href="#">Voltar para o Topo</a></p>
-                <p>&copy; 2015 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+                <p>&copy; 2015 Amarilis Camargo &middot;</p>
               </footer>
 		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 	    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
